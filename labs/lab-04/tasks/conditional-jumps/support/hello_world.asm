f350ab181cd2f420686f691865a2ccd9b807064c
leosmk5 <leocurcan@gmail.com>
2026-03-26 11:08:53 +0000

    a

--- labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
+++ labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
@@ -4,6 +4,7 @@
 
 section .data
     myString: db "Hello, World!", 0
+    goodbyeString: db "Goodbye, World!", 0
     N: dq 6                         ; N = 6
 
 section .text
@@ -20,7 +21,7 @@ main:
     mov rax, 2
     mov r8, 1
     cmp rax, r8
-    je print                        ; TODO1: rax > r8?
+    jg print                        ; TODO1: rax > r8?
     xor rax, rax
 
     leave
@@ -32,5 +33,10 @@ print:
                                     ; TODO2.1: print "Goodbye, World!"
     xor rax, rax
 
+start: 
+    cmp rax, rcx
+    PRINTF64 `%s\n\x0`, myString
+    add rax, rax
+    jnz start
     leave
     ret
