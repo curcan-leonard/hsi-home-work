29291fe63df72c35b4f0bdc8d338abd641bdf07b
leosmk5 <leocurcan@gmail.com>
2026-04-30 09:21:16 +0000

    Fibonacci

--- labs/lab-04/tasks/fibonacci/support/fibonacci.asm
+++ labs/lab-04/tasks/fibonacci/support/fibonacci.asm
@@ -14,15 +14,23 @@ main:
     mov rbp, rsp
 
     mov rcx, QWORD [N]       ; we want to find the N-th fibonacci number; N = RCX = 7
+    ; TODO: calculate the N-th fibonacci number (f(0) = 0, f(1) = 1)
     PRINTF64 `%d\n\x0`, rcx  ; DO NOT REMOVE/MODIFY THIS LINE
 
-    ; TODO: calculate the N-th fibonacci number (f(0) = 0, f(1) = 1)
+    mov rax, 0
+    mov r9, 1
 
+fibonacci:
+    dec rcx
+    test rcx, rcx
+    je print
+    add rax, r9
+    xchg rax, r9
+    jmp fibonacci
+
+print:
+    PRINTF64 `%d\n\x0`, r9
     xor rax, rax
 
     leave
     ret
-    
-start:
-    add rax, 1
-    mov r8
